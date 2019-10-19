88a67f47434f1f2c426c2bd50ab9fabb4cb790b0
SmellBells <elliottgonzales@elliotts-MacBook-Pro.local>
2019-10-19 14:28:35 -0600

    Added Components App, Header, NotFound, Order, Inventory, StorePicker,and Router

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Inventory extends Component {
+  render() {
+    return (
+      <div className="inventory"> 
+        Inventory man!
+      </div>
+    );
+  }
+}
+
+export default Inventory;
---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h2>Page Not Found ¯\_(ツ)_/¯ </h2>
+  </div>
+);
+
+export default NotFound;
