88e25dfb9aed247a411ae5cc789420fe8547f4c8
SmellBells <elliottgonzales@elliotts-MacBook-Pro.local>
2019-10-25 13:43:06 -0600

    Worked on StorePicker some more, got it to take the random url and go to main page of site

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    const StoreName = this.myInput.current.value;
+    // 3. Change the page to whatever the user entered in
+    this.props.history.push(`/store/${StoreName}`);
+  };
   render() {
       return (
-        <form ClassName="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name"/>
+          <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()}/>
           <button type="submit">Visit Store →</button>
         </form>      
       );
